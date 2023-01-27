cf607ef146327ac6ecc62d3d260a2e76581a2b8b
sh-bd <shamim_ice1202@std.bauet.ac.bd>
2023-01-27 23:06:48 +0600

    done

--- practice-2.js
+++ practice-2.js
@@ -9,86 +9,86 @@ var johnsTotal = 40;
  * 60 or above b
  * 50 or above c
  * 40 or above d
- * 39 => f
+ * 39 >= f
  */
 
 // MY RESULT
-if (myTotal => 80) {
+if (myTotal >= 80) {
     console.log('I got A grade');
 }
-else if (myTotal => 60) {
+else if (myTotal >= 60) {
     console.log('I got B grade');
 }
-else if (myTotal => 50) {
+else if (myTotal >= 50) {
     console.log('I got C grade');
 }
-else if (myTotal => 40) {
+else if (myTotal >= 40) {
     console.log('I got D grade');
 }
-else if (myTotal <= 39) {
+else {
     console.log('I got F grade');
 }
 // TOM'S RESULT
-if (tomsTotal => 80) {
+if (tomsTotal >= 80) {
     console.log('Tom got A grade');
 }
-else if (tomsTotal => 60) {
+else if (tomsTotal >= 60) {
     console.log('Tom got B grade');
 }
-else if (tomsTotal => 50) {
+else if (tomsTotal >= 50) {
     console.log('Tom got C grade');
 }
-else if (tomsTotal => 40) {
+else if (tomsTotal >= 40) {
     console.log('Tom got D grade');
 }
-else if (tomsTotal <= 39) {
+else {
     console.log('Tom got F grade');
 }
 // JANE'S RESULT
-if (janesTotal => 80) {
+if (janesTotal >= 80) {
     console.log('Jane got A grade');
 }
-else if (janesTotal => 60) {
+else if (janesTotal >= 60) {
     console.log('Jane got B grade');
 }
-else if (janesTotal => 50) {
+else if (janesTotal >= 50) {
     console.log('Jane got C grade');
 }
-else if (janesTotal => 40) {
+else if (janesTotal >= 40) {
     console.log('Jane got D grade');
 }
-else if (janesTotal <= 39) {
+else {
     console.log('Jane got F grade');
 }
 // PETER'S RESULT
-if (petersTotal => 80) {
+if (petersTotal >= 80) {
     console.log('Peter got A grade');
 }
-else if (petersTotal => 60) {
+else if (petersTotal >= 60) {
     console.log('Peter got B grade');
 }
-else if (petersTotal => 50) {
+else if (petersTotal >= 50) {
     console.log('Peter got C grade');
 }
-else if (petersTotal => 40) {
+else if (petersTotal >= 40) {
     console.log('Peter got D grade');
 }
-else if (petersTotal <= 39) {
+else {
     console.log('Peter got F grade');
 }
 // JOHN'S RESULT
-if (johnsTotal => 80) {
+if (johnsTotal >= 80) {
     console.log('John got A grade');
 }
-else if (johnsTotal => 60) {
+else if (johnsTotal >= 60) {
     console.log('John got B grade');
 }
-else if (johnsTotal => 50) {
+else if (johnsTotal >= 50) {
     console.log('John got C grade');
 }
-else if (johnsTotal => 40) {
+else if (johnsTotal >= 40) {
     console.log('John got D grade');
 }
-else if (johnsTotal <= 39) {
+else {
     console.log('John got F grade');
 }
